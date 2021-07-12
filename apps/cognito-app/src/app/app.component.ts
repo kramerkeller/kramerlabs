b485cb85db02b09a5b0edbbb95d922520081348e
Kramer Keller <kramerkeller@gmail.com>
2021-07-12 18:58:44 -0400

    initial commit

---
+++ apps/cognito-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kramerlabs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cognito-app';
+}
